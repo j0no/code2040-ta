3f942eda2dbc9c22a7bb5038c57ead945c9a044b
Jonathan <classforlan@gmail.comremote: Permission to ClassForLan/class-app.git denied to jmarcelsw>
2016-08-18 01:37:58 -0700

    step_3 works

---
+++ step_3/README.md
@@ -0,0 +1,6 @@
+# Step 3: Needle in a Haystack
+Requirements:
+
+- Send POST with token
+- Find Index of Needle in haystack
+- Send POST with the index and token
---
+++ step_3/app.js
@@ -0,0 +1,59 @@
+// Node Module
+const request = require("../node_modules/request-promise");
+
+const getIndexOfStringInArray = (array, value) => {
+    const index = array.findIndex((element, i, cArray) => {
+        switch (element) {
+            case value:
+                return true;
+                break;
+            default:
+                return false;
+        }
+    })
+    console.log("The needle (", value, ") is at index: ", index, " in the haystack")
+    return index;
+}
+
+const send = (index) => {
+    const rOptions = {
+        method: "POST",
+        uri: "http://challenge.code2040.org/api/haystack/validate",
+        body: {
+            "token": "token please",
+            "needle": index
+        },
+        json: true
+    };
+    const respondReq = request(rOptions);
+    respondReq
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+// Ask for array from api
+
+const aOptions = {
+    method: "POST",
+    uri: "http://challenge.code2040.org/api/haystack",
+    body: {
+        "token": "token please"
+    },
+    json: true
+};
+
+request(aOptions)
+    .then(hay => {
+        console.log("Haystack: ", hay.haystack, " , Needle: ", hay.needle);
+
+        const index = getIndexOfStringInArray(hay.haystack, hay.needle);
+
+        send(index);
+    })
+    .catch(error => {
+        console.log(error)
+    })
