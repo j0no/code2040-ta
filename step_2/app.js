c4f4284d001b967743875e22981bdd7d1e833fb3
Jonathan <classforlan@gmail.comremote: Permission to ClassForLan/class-app.git denied to jmarcelsw>
2016-08-18 00:01:03 -0700

    step_2 works

--- step_2/app.js
+++ step_2/app.js
@@ -1,12 +1,51 @@
-/* Node Module
-const request = require('../node_modules/request-promise');
+// Node Module
+const request = require("../node_modules/request-promise");
 
-const ask = new request();
-const respond = new request(); */
 
-const reverse = (word) => {
-    return word.split("").reverse().join("");
+const reverse = (word) =>  {
+    const rWord = word.split("").reverse().join("");
+    console.log("Reversed Word: ", rWord);
+    return rWord;
 }
 
-console.log(reverse("ass"));
+const send = (reversedWord) =>  {
+    const rOptions = {
+        method: "POST",
+        uri: "http://challenge.code2040.org/api/reverse/validate",
+        body: {
+            "token" : "015e760aa9c5abf8a22a817f54ca248e",
+            "string" : reversedWord
+        },
+        json: true
+    };
+    const respondReq = request(rOptions);
+    respondReq
+        .then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
 
+// Ask for word from api
+
+const aOptions = {
+    method: "POST",
+    uri: "http://challenge.code2040.org/api/reverse",
+    body: {
+        "token" : "015e760aa9c5abf8a22a817f54ca248e"
+    },
+    json: true
+};
+
+request(aOptions)
+    .then(word => {
+        console.log("Word: ", word);
+        const rWord = reverse(word);
+        // Validate
+        send(rWord);
+    })
+    .catch(error => {
+        console.log(error)
+    })
